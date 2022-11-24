d630cf97badd11717d831822435f7803721c9b1f
KhalykMadi <kh_madi00@mail.ru>
2022-11-24 12:12:53 +0600

    master

--- js/script.js
+++ js/script.js
@@ -13,11 +13,49 @@ $(document).ready(function() {
   })
 });
 
-
+// Modal Youtube Jquery
+$(".js-video-button").modalVideo();
 
 // SLIDERS
 // 1. OWNERS
 var ownersSwiper = new Swiper(".ownersSwiper", {
+  spaceBetween: 30,
+  loop: 0,
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-1",
+    prevEl: ".swiper-button-prev-1",
+  },
+});
+// 2. COFOUNDER
+var cofounderSwiper = new Swiper(".cofounderSwiper", {
+  spaceBetween: 30,
+  loop: 0,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next-2",
+    prevEl: ".swiper-button-prev-2",
+  },
+});
+// 1. OWNERS
+var ownersSwiper = new Swiper(".ownersSwiper2", {
   spaceBetween: 30,
   loop: 0,
   breakpoints: {
@@ -72,4 +110,19 @@ var swiper1 = new Swiper(".mainSwiper", {
     },
   },
   autoHeight: true,
+});
+
+$(window).on("scroll", function(e) {
+  if ($(this).scrollTop() > 0) {
+      $('.js-back-to-top').fadeIn('slow');
+  } else {
+      $('.js-back-to-top').fadeOut('slow');
+  }
+});
+
+$(".js-back-to-top").on("click", function(e) {
+  $("html, body").animate({
+      scrollTop: 0
+  }, 100);
+  return false;
 });
